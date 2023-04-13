1fa1918847790bc9ad07673eb2b1cccf39c6c3e8
SAISUDHAKAR <99793661+SaisudhakarBhaskaruni@users.noreply.github.com>
2023-04-13 08:57:32 +0530

    JAVA
    By doing this project base up on java  and database to register the seats,, and details of bus ,admin ,bus number,price ,admin slot and user slot etc.......

---
+++ BusBookingGui.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package busbookinggui;
+
+/**
+ *
+ * @author 16903
+ */
+public class BusBookingGui {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     // todo code appliacation logic here 
+     mainScreen mr =new mainScreen();
+     mr.setLocationRelativeTo(null);
+     mr.setVisible(true);
+     
+    }
+    
+}
